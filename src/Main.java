45a1c0625c4d13ada07efbe3811cc5514927fc6c
Sofia Taskina <sofiya.9691@gmail.com>
2026-01-26 15:39:16 +0300

    test2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
@@ -13,10 +10,16 @@ public class Main {
         }
 
         print();
+        test2();
     }
 
     public static void print()
     {
         System.out.println("Print print PRINT");
+    }
+
+    public static void test2()
+    {
+        System.out.println("PRINT TEST 2");
     }
 }
